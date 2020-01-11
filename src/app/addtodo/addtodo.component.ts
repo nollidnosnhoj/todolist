3cee4c3d923f5dea5fb52256275e514b8e69e8ed
Dillon Johnson <nollidnosnhoj@gmail.com>
2020-01-11 10:03:43 -1000

    add form component

--- src/app/addtodo/addtodo.component.ts
+++ src/app/addtodo/addtodo.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { TodoItem } from '../models/todo.model';
+import { TodolistService } from '../services/todolist.service';
 
 @Component({
   templateUrl: './addtodo.component.html',
@@ -6,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTodoComponent implements OnInit {
 
-  constructor() { }
+  addForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private todolistService: TodolistService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.addForm = this.fb.group({
+      title: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+      content: new FormControl('', [Validators.maxLength(500)])
+    });
+  }
+
+  get f() { return this.addForm.controls; }
+
+  addItem() {
+    if (this.addForm.invalid) return;
+
+    const newTodo: TodoItem = {
+      ...this.addForm.getRawValue(),
+      id: ''
+    };
+
+    this.todolistService.addItem(newTodo);
+
+    this.router.navigate(['/']);
   }
 
 }
